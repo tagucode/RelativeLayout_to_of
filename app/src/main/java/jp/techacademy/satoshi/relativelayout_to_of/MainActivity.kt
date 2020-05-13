9ab9b5d4506342fe8d3d1770148b3e94c1054017
tagucode <silverxclock@gmail.com>
2020-05-14 03:06:56 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/satoshi/relativelayout_to_of/MainActivity.kt
@@ -0,0 +1,12 @@
+package jp.techacademy.satoshi.relativelayout_to_of
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
